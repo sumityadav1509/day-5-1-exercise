f254726a0ac83bf0461ed44c57203be0c94d6db8
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 20:37:49 +0000

    Complete code to calculate average height.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-# 🚨 Don't change the code below 👇
+# 🚨 Input the heights below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
+# 🚨 Made a list above 👆
+print(student_heights)
+
+#Code is below this row 👇
+total_height=0
+for height in student_heights: 
+  total_height+=height 
+
+print(total_height) 
+
+number_of_students=0 
+for student in student_heights:
+  number_of_students+=1 
+
+print(number_of_students) 
 
 
-#Write your code below this row 👇
+for average in student_heights:
+  average_height=round(total_height/number_of_students) 
+
+print(average_height)  
 
 
 
