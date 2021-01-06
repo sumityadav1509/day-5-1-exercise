957a065d7c8f711c0ce8a21acbdb97b149826262
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 21:54:43 +0000

    Completed the fizz-buzz exercise

--- main.py
+++ main.py
@@ -3,11 +3,27 @@
 #  total+=number
 
 # print(total) 
-sum_of_even_number=0
-for number in range(1,101):
-  if number%2==1:
-    # print(number) 
-    sum_of_even_number+=number 
+# sum_of_even_number=0
+# for number in range(1,101):
+#   if number%2==0:
+#     # print(number) 
+#     sum_of_even_number+=number 
 
-print(f"The sum of all numbers that are even is :{sum_of_even_number}")
+
+# print(f"The sum of all numbers that are even is :{sum_of_even_number}")
   
+  # Fizz Buzz Code : 
+
+for number in range(1,19) :
+  # Mistake was made here as i had forgot to check that both the conditions should be true at the intitial start rather than towards the end.
+  if number%3==0 and number%5==0:
+     print("FizzBuzz")
+  elif number%5==0:
+     print("Buzz")   
+  elif number%3==0:
+      print("Fizz") 
+  else:
+    print(number)    
+    
+    
+        
