92c4fe191e2145474270dc5a150e14e761540443
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 21:32:11 +0000

    Sum of all even number exercise completed.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-total=0
-for number in range(1,101):
- total+=number
+# total=0
+# for number in range(1,101):
+#  total+=number
 
-print(total)
+# print(total) 
+sum_of_even_number=0
+for number in range(1,101):
+  if number%2==1:
+    # print(number) 
+    sum_of_even_number+=number 
+
+print(f"The sum of all numbers that are even is :{sum_of_even_number}")
+  
