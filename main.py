a9b6a883da6b2652a4f4b7e43b67b2fc0ff182ab
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 21:21:56 +0000

    Sum of numbers from 1-100 .

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-# Marks input by students 
+total=0
+for number in range(1,101):
+ total+=number
 
-student_scores=input("Input a list of student scores").split()  
-# print(len(student_scores))
-# Forgot to use the split function upwards as it is required to split the data to be placed inside a list below.
-for n in range(0,len(student_scores)):
-  student_scores[n]=int(student_scores[n]) 
-
-print(student_scores) 
-highest_score=0
-for score in student_scores:
-  
-  if score>highest_score:
-    highest_score=score 
-    print(f"The highest score is : {highest_score}")
+print(total)
